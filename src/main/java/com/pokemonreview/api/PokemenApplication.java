f4083b84154244eb43114c4040e5ee56b3dc2452
hadt25 <hadt25@ghtk.co>
2024-01-03 11:23:28 +0700

    init

---
+++ src/main/java/com/pokemonreview/api/PokemenApplication.java
@@ -0,0 +1,13 @@
+package com.pokemonreview.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemenApplication.class, args);
+    }
+
+}
